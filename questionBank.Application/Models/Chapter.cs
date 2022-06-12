ea15976c2ba9f86b9828da61d95d46eaf4b3a11c
golam habib palash <golamhabibpalash@hotmail.com>
2022-06-13 00:40:22 +0600

    Chapter Create page modified

--- questionBank.Application/Models/Chapter.cs
+++ questionBank.Application/Models/Chapter.cs
@@ -1,8 +1,13 @@
-﻿namespace questionBank.Application.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace questionBank.Application.Models
 {
     public class Chapter : CommonProps
     {
+        [Display(Name ="Chapter Name")]
         public string ChapterName { get; set; } = string.Empty;
+
+        [Display(Name = "Chapter Number")]
         public int ChapterNumber { get; set; }
         public int AcademicSubjectId { get; set; }
         public AcademicSubject? AcademicSubject { get; set; }
